fb830bd8b6531508fa40cd8f224e0d0068408d12
gdweijin <gdweijin2008@163.com>
2015-12-22 10:52:43 +0800

    gg

--- src/泛型/GenericsDemo06.java
+++ src/泛型/GenericsDemo06.java
@@ -17,5 +17,6 @@ public class GenericsDemo06 {
 		Point<String> p = new Point<String>(); // 里面的var类型为String类型
 		p.setVar("it"); // 设置字符串
 		System.out.println(p.getVar().length()); // 取得字符串的长度
+		System.out.print("git test");
 	}
 };
