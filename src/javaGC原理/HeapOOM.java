08ec75dc0d0e07e48cb25eeb21e90731e41999bd
gdweijin <gdweijin2008@163.com>
2016-03-07 19:24:42 +0800

    commit

---
+++ src/javaGC原理/HeapOOM.java
@@ -0,0 +1,22 @@
+package javaGC原理;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+* @ClassName: HeapOOM 
+*
+ */
+public class HeapOOM {
+    static class OOMObject{
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while(true){
+            list.add(new OOMObject());
+        }
+    }
+}
