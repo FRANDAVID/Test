5234fe51e6c046c76e9deb2a778c431031cace82
wei jin <weijin1@lenovo.com>
2018-08-08 14:24:20 +0800

    stashtest

---
+++ src/leetcode2/TwoSum.java
@@ -0,0 +1,17 @@
+package leetcode2;
+
+/**
+ * Company lenovo.com
+ * Copyright (C) 1984-2018 All Rights Reserved.
+ *
+ * @author david
+ * @version TwoSum.java, v 0.1 2018-07-18 上午11:33 david
+ * @project Test
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] i
+    }
+
+
+}
---
+++ src/重入锁/RedisWithReentrantLock.java
@@ -0,0 +1,55 @@
+package 重入锁;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RedisWithReentrantLock {
+
+  private ThreadLocal<Map<String, Integer>> lockers = new ThreadLocal<>();
+
+  private Jedis jedis;
+
+  public RedisWithReentrantLock(Jedis jedis) {
+    this.jedis = jedis;
+  }
+
+  private boolean _lock(String key) {
+    return jedis.set(key, "", "nx", "ex", 5L) != null;
+  }
+
+  private void _unlock(String key) {
+    jedis.del(key);
+  }
+
+  private Map<String, Integer> currentLockers() {
+    Map<String, Integer> refs = lockers.get();
+    if (refs != null) {
+      return refs;
+    }
+    lockers.set(new HashMap<>());
+    return lockers.get();
+  }
+
+  public boolean lock(String key) {
+    Map<String, Integer> refs = currentLockers();
+    Integer refCnt = refs.get(key);
+    if (refCnt != null) {
+      refs.put(key, refCnt + 1);
+      return true;
+    }
+    boolean ok = this._lock(key);
+    if (!ok) {
+      return false;
+    }
+    refs.put(key, 1);
+    return true;
+  }
+
+  public boolean unlock(String key) {
+    Map<String, Integer> refs = currentLockers();
+    Integer refCnt = refs...
+
+https://juejin.im
+掘金 — 一个帮助开发者成长的社区
